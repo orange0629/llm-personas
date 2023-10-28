33e2d79a4117f5a71f5cee817bc2eb7f3d00489c
bangzhaojo <93449336+bangzhaojo@users.noreply.github.com>
2023-10-27 23:18:41 -0400

    Some examples of running new scripts

--- src/prompt-variation/test.sh
+++ src/prompt-variation/test.sh
@@ -1,3 +1,12 @@
-python model_prompting_script.py -m "cardiffnlp/twitter-roberta-base" -scale "ACI"    # example for masked models
+# TEST MODELS WITH PROMPTS
 
-python model_prompting_script.py -m "EleutherAI/gpt-neo-1.3B" -scale "ACI" --causal   # example for causal models
+# an example for masked models
+python model_prompting_script.py -m "cardiffnlp/twitter-roberta-base" -scale "ACI" --accelerate
+
+# examples for causal models
+
+python model_prompting_script.py -m "/shared/2/projects/llm-personas/llama2-bible/checkpoint-5000" -scale "SIPS_new" --causal -BATCH 32
+python model_prompting_script.py -m "gpt2" -scale "SIPS_new" --causal -BATCH 32
+
+# an example for LoRA
+python model_prompting_script.py -m "/shared/2/projects/llm-personas/llama2-4chan-lora/checkpoint-500" -scale "EPQ_new" --causal -BATCH 32 --lora
